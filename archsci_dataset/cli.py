5d29c2ef50f14978fd5af9f1ee0c570ffe2aa16d
Jason Zou <jason.zou@gmail.com>
2025-02-20 15:41:36 -0500

    poe poetry

--- archsci_dataset/cli.py
+++ archsci_dataset/cli.py
@@ -1,6 +1,6 @@
 # archsci-dataset/cli.py
 
-from typing import Optional, Annotated
+from typing import Annotated
 import typer
 from pathlib import Path
 
@@ -16,40 +16,65 @@ def _version_callback(value: bool = True) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
-
-@app.callback()
-def main(
-    root: Annotated[
+    
+@app.command("convert")
+def convert(output_dir: Annotated[
         Path,
-        typer.Option(
-            help="The project root directory.",
+        typer.Option( 
+            "--destionation", 
+            "-d", 
+            help="Output direcotry for processed data",
             dir_okay=True,
             writable=True,
             resolve_path=True,
-        ),
+        )
     ]="./output",
-    force: Annotated[
-        bool,
-        typer.Option(help="Force initialization even if the project already exists."),
-    ] = False,
-    version: Annotated[bool, 
-        typer.Option("--version", "-v", help="Show the application's version and exit.", callback=_version_callback, is_eager=True,
-    )]=False,
-    # output_dir: Optional[str] = typer.Option(
-    #     "output",
-    #     "--destionation",
-    #     "-d",
-    #     help="Output direcotry for processed data",
-    #     is_eager=True,
-    # ),
-    # endpoint: Optional[str] = typer.Option(
-    #     "http://localhost:11434",
-    #     "--endpoint",
-    #     "-e",
-    #     help="Ollama API endpoint",
-    #     is_eager=True,
-    # )
+    endpoint: Annotated[
+        str, 
+        typer.Option( 
+            "--endpoint", 
+            "--endpoint", 
+            "-e", 
+            help="Ollama API endpoint", 
+        )
+    ]="http://localhost:11434", 
 ) -> None:
-    # print(output_dir)
-    # print(endpoint)
+    print(endpoint)
+    print(output_dir)
+    print("jkfld;afsd")
+    return
+
+@app.callback()
+def main(
+    version: Annotated[
+        bool, 
+        typer.Option(
+            "--version", 
+            "-v", 
+            help="Show the application's version and exit.", 
+            callback=_version_callback, 
+            is_eager=True,
+    )]=False,
+    output_dir: Annotated[
+        Path,
+        typer.Option( 
+            "--destionation", 
+            "-d", 
+            help="Output direcotry for processed data",
+            dir_okay=True,
+            writable=True,
+            resolve_path=True,
+        )
+    ]="./output",
+    endpoint: Annotated[
+        str, 
+        typer.Option( 
+            "--endpoint", 
+            "-e", 
+            help="Ollama API endpoint", 
+        )
+    ]="http://localhost:11434", 
+) -> None:
+    print(output_dir)
+    print(endpoint)
     return
